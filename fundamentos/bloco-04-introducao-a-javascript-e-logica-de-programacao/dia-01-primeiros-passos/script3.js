8861a7c1d8eb0a804c446a32b1edfd757002db23
DanielRohe86 <psicologo.rohe@gmail.com>
2022-05-12 17:52:53 -0300

    adiciona arquivos iniciais do módulo 4.1

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-primeiros-passos/script3.js
@@ -0,0 +1,15 @@
+const notaDesafio = 59;
+
+if (notaDesafio >= 80){
+    console.log("Parabéns, você foi aprovada(o)!");
+}
+else if (notaDesafio < 80 && notaDesafio >= 60) {
+    console.log("Você está na nossa lista de espera");
+}
+else {
+    console.log("Você foi reprovada(o)")
+}
+
+console.log(notaDesafio);
+
+//if (notaDesafio < 60)
