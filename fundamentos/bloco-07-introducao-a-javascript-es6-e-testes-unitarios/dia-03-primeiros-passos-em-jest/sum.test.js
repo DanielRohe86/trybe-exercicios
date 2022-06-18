933f4f18ad088dbdf406064301471d3ea825e1b9
DanielRohe86 <psicologo.rohe@gmail.com>
2022-06-18 17:54:13 -0300

    finaliza uploads de sábado

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
@@ -1,4 +1,4 @@
-const sum = require('./sum.js');
+const sumTests = require('./sum.js');
 
 test('adds 4 and 5 equals 9', () => {
   expect(4 + 5).toEqual(9);
@@ -8,8 +8,43 @@ test('adds 0 and 0 equals 0', () => {
   expect(0 + 0).toEqual(0);
 });
 
+//------------------------------------------
+
 test('check if parameters are not numbers', () => {
-  expect(typeOf(a) && typeOf(b)).toBe(typeOf(number));
+  expect(() => {
+    sumTests.sumNumbers(4, "5"); 
+    }).toThrow();    
+  });
+
+//------------------------------------------ 
+
+test('check if new Array does not include 3', () => {
+  expect(() => {
+    sumTests.myRemove([1, 2, 3, 4], 3);
+    (newArr).toContain([1, 2, 4]);
+  });
 });
 
+test('check if new Array does not return 1, 2, 3, 4', () => {
+  expect(() => {
+    sumTests.myRemove([1, 2, 3, 4], 3);
+    (newArr).not.toContain([1, 2, 3, 4]);
+  });
+});
+
+test('check if new Array returns 1, 2, 3, 4', () => {
+  expect(() => {
+    sumTests.myRemove([1, 2, 3, 4], 5);
+    (newArr).toContain([1, 2, 3, 4]);
+  });
+});
+
+//------------------------------------------ 
+//ERRO
+test('check if 15 returns fizzbuzz'), () => {
+  sumTests.myFizzBuzz(15);
+  expect(() => {
+  expect(15).toEqual('fizzbuzz');
+});
+};
 
