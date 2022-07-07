fc9297b0878732495b59ff1385078751e4fddfeb
DanielRohe86 <psicologo.rohe@gmail.com>
2022-07-07 20:57:50 -0300

    adiciona arquivos iniciais

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
@@ -1,50 +1,65 @@
 const sumTests = require('./sum.js');
 
-test('adds 4 and 5 equals 9', () => {
-  expect(4 + 5).toEqual(9);
-});
+describe('The function sum', () => {
 
-test('adds 0 and 0 equals 0', () => {
-  expect(0 + 0).toEqual(0);
+  test('adds 4 and 5 equals 9', () => {
+    expect(4 + 5).toEqual(9);
+  });
+
+  test('adds 0 and 0 equals 0', () => {
+    expect(0 + 0).toEqual(0);
+  });
 });
 
 //------------------------------------------
 
-test('check if parameters are not numbers', () => {
-  expect(() => {
-    sumTests.sumNumbers(4, "5"); 
-    }).toThrow();    
+describe('The function sumTests', () => {
+
+  test('check if parameters are not numbers', () => {
+    expect(() => {
+      sumTests.sumNumbers(4, "5"); 
+      }).toThrow();    
+    });
   });
 
-//------------------------------------------ 
 
-test('check if new Array does not include 3', () => {
-  expect(() => {
-    sumTests.myRemove([1, 2, 3, 4], 3);
-    (newArr).toContain([1, 2, 4]);
+describe('The function myRemove', () => {
+
+  test('check if new Array does not include 3', () => {
+    expect(() => {
+      sumTests.myRemove([1, 2, 3, 4], 3);
+      (newArr).toContain([1, 2, 4]);
+    });
+  });
+
+  test('check if new Array does not return 1, 2, 3, 4', () => {
+    expect(() => {
+      sumTests.myRemove([1, 2, 3, 4], 3);
+      (newArr).not.toContain([1, 2, 3, 4]);
+    });
+  });
+
+  test('check if new Array returns 1, 2, 3, 4', () => {
+    expect(() => {
+      sumTests.myRemove([1, 2, 3, 4], 5);
+      (newArr).toContain([1, 2, 3, 4]);
+    });
   });
 });
-
-test('check if new Array does not return 1, 2, 3, 4', () => {
-  expect(() => {
-    sumTests.myRemove([1, 2, 3, 4], 3);
-    (newArr).not.toContain([1, 2, 3, 4]);
-  });
-});
-
-test('check if new Array returns 1, 2, 3, 4', () => {
-  expect(() => {
-    sumTests.myRemove([1, 2, 3, 4], 5);
-    (newArr).toContain([1, 2, 3, 4]);
-  });
-});
-
 //------------------------------------------ 
 //ERRO
-test('check if 15 returns fizzbuzz'), () => {
-  sumTests.myFizzBuzz(15);
-  expect(() => {
-  expect(15).toEqual('fizzbuzz');
-});
+//Missing second argument. It must be a callback function. Perhaps you want to use `test.todo` for a test placeholder.
+
+/* describe('The function fizzbuzz', () => {
+ */
+  test('check if 15 returns fizzbuzz'), () => {
+    expect(() => {
+    sumTests.myFizzBuzz('15').toEqual('fizzbuzz');
+    /*  sumTests.myFizzBuzz();
+    expect('15').toEqual('fizzbuzz'); */
+  });
 };
+
+//------------------------------------------ 
+
 
