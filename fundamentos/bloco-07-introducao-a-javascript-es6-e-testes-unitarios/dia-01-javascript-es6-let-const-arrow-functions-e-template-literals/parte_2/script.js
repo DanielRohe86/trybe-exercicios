715922947bcb65b9fe2f4189c2b264a5077fc545
DanielRohe86 <psicologo.rohe@gmail.com>
2022-07-17 21:39:38 -0300

    último commit do dia

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/parte_2/script.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/parte_2/script.js
@@ -159,4 +159,28 @@ console.log(longestWord('Antonio foi ao banheiro e não sabemos o que aconteceu'
 //ESTÁ NO HTML
 
 //EXERCÍCIO 4
+const substituaX = (nome) => {
+  const frase = 'Tryber x aqui!';
+  const fraseArray = frase.split(' ');
+  for (let index = 0; index < fraseArray.length; index += 1) {
+    if (fraseArray[index] === 'x') {
+      fraseArray[index] = nome;
+    }
+  }  
+  return fraseArray.join(' ');
+};
 
+console.log(substituaX('Bebeto'));
+
+const minhasSkills = (func) => {
+  const skills = ['JavaScript', 'HTML', 'CSS'];
+  let resultado = 'Minhas três principais habilidades são:';
+
+  skills.forEach((skill) => { // estrutura de repetição para iterar sobre o array de skills
+    resultado = `${resultado}
+    - ${skill}`;
+  }); // reatribui e adiciona a skill atual a variável resultado
+  return resultado;
+};
+
+console.log(minhasSkills(substituaX('Bebeto')));
